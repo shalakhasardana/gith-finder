848251e1a9564a324533b7ac8eeffbb569233dec
unknown <shalakhasardana18@gmail.com>
2020-09-23 07:42:30 -0700

    Evaluate arrow function inside render

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 
 class App extends Component {
   render() {
-    const name = 'Shalakha';
+    const name = () => 'Bar';
     return (
       <div>
-        <h1>Hello {name.toLowerCase()}</h1>
+        <h1>Hello {name().toLowerCase()}</h1>
       </div>
     );
   }
