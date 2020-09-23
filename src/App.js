fa26837c6bc453e78d355398dfac0cf6c2e64e71
unknown <shalakhasardana18@gmail.com>
2020-09-23 07:50:39 -0700

    add multple ternanry operator

--- src/App.js
+++ src/App.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  loading = true;
+  loading = false;
+  showName = false;
+  name = 'shalakha';
 
   render() {
-    return <div>{this.loading ? <h1>Loading....</h1> : <h1>Hello</h1>}</div>;
+    return (
+      <div>
+        {this.loading ? (
+          <h1>Loading....</h1>
+        ) : (
+          <h1>Hello {this.showName ? this.name : null}</h1>
+        )}
+      </div>
+    );
   }
 }
 
