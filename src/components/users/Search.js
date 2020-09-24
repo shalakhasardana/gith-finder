6dc1147daeef8255aa722e0089224dfa5329529a
unknown <shalakhasardana18@gmail.com>
2020-09-24 01:16:28 -0700

    Pass function prop types

---
+++ src/components/users/Search.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class Search extends Component {
+  state = {
+    text: '',
+  };
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+  };
+
+  handleChange = (e) => {
+    this.setState({ text: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.searchUsers(this.state.text);
+    this.setState({ text: '' });
+  };
+
+  render() {
+    return (
+      <div>
+        <form className='form' onSubmit={this.handleSubmit}>
+          <input
+            type='text'
+            name='text'
+            placeholder='Search Users....'
+            value={this.state.text}
+            onChange={this.handleChange}
+          />
+          <input
+            type='submit'
+            value='Search'
+            className='btn btn-dark btn-block'
+          />
+        </form>
+      </div>
+    );
+  }
+}
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,57 +1,26 @@
-import React, { Component } from 'react';
+import React from 'react';
 import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
+import PropTypes from 'prop-types';
 
-class Users extends Component {
-  state = {
-    users: [
-      {
-        login: 'mojombo',
-        id: 1,
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo',
-      },
-      {
-        login: 'abc',
-        id: 2,
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo',
-      },
-      {
-        login: 'shalakha',
-        id: 3,
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo',
-      },
-      {
-        login: 'rinkey',
-        id: 4,
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo',
-      },
-      {
-        login: 'mohit',
-        id: 5,
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo',
-      },
-      {
-        login: 'efg',
-        id: 6,
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo',
-      },
-    ],
-  };
-  render() {
-    return (
-      <div style={userStyle}>
-        {this.state.users.map((user) => (
-          <UserItem key={user.id} user={user} />
-        ))}
-      </div>
-    );
+const Users = ({ loading, users }) => {
+  if (loading) {
+    return <Spinner />;
   }
-}
+
+  return (
+    <div style={userStyle}>
+      {users.map((user) => (
+        <UserItem key={user.id} user={user} />
+      ))}
+    </div>
+  );
+};
+
+Users.prototype = {
+  users: PropTypes.array.isRequired,
+  loading: PropTypes.bool.isRequired,
+};
 
 const userStyle = {
   display: 'grid',
