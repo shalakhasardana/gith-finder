60ecc830855fefc9e0428a5845c39e8054086235
unknown <shalakhasardana18@gmail.com>
2020-09-23 18:26:57 -0700

    array destructure and apply proptypes

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Navbar = (props) => {
+const Navbar = ({ icon, title }) => {
   return (
     <nav className='navbar bg-primary'>
       <h1>
-        <i className={props.icon} />
-        {this.props.title}
+        <i className={icon} />
+        {title}
       </h1>
     </nav>
   );
